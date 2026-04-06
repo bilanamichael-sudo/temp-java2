708b941fe585114502180705a795256b1203742a
michaelbilaña <bilanamichael@gmail.coma>
2026-04-06 09:41:22 -0500

    ingrese edad

--- main.java
+++ main.java
@@ -1,7 +1,23 @@
+import java.util.*;
+import javax.swing.JOptionPane;
 
 public class main{
+    
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+      int edad;
+      Scanner entrada = new Scanner(System.in);
+
+
+
+        System.out.println("Ingrese la edad: ");
+        edad = entrada.nextInt();
+
+        if(edad < 18){
+            JOptionPane.showMessageDialog(null, "Es menor de edad");
+
+        }else{
+            JOptionPane.showMessageDialog(null, "Es mayor de edad");
+        }
     }
 }
